1dede4cd4f6934ef545a2f429cb086f4d3841a54
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-10-07 13:48:11 +0800

    feat(Dashboard): add dashboard placeholder using dorayaki list

--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,12 +9,25 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/list.css">
     <script src="../components/navbar.js" type="text/javascript" defer></script>
+    <script src="../components/list-card.js" type="text/javascript" defer></script>
     <title>Document</title>
 </head>
+
 <body>
     <?php
         echo "<navbar-component></navbar-component>";
     ?>
+    <div class="container">
+        <h2 class="page-header">Top 10 Dorayaki</h2>
+        <?php
+            for ($i=0;$i<10;$i++) {
+                $isFinalIndex = $i == 9;
+                echo "<list-card final=$isFinalIndex></list-card>";
+            }
+        ?>
+    </div>
 </body>
+
 </html>
