e2a0d34199417f25b92b5489cf30f6fb0c4d0e3d
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-10-07 12:45:36 +0800

    feat(List): add dorayaki list / search result page

---
+++ components/list-card.js
@@ -0,0 +1,76 @@
+class ListCard extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const isFinalIndex = this.getAttribute("final");
+
+        this.innerHTML = `
+            <a class="card-href" href="detail.php">
+                <div class=${
+                    isFinalIndex ? "card-container-final" : "card-container"
+                }>
+                    <img class="card-thumbnail" src="https://asset.kompas.com/crops/8mYWlI9lPaf8F7XDmQOi2Rte9jo=/0x0:1000x667/750x500/data/photo/2021/07/23/60fa5f58ea527.jpg" alt="Avatar"/>
+                    <div style="flex: 1;">
+                        <h4 class="card-title">Doroyaki Norimitsu</h4>
+                        <p class="card-desc">Dora The Explorer senang makan Dorayaki</p>
+                        <p class="card-sold">Terjual: 15 buah</p>
+                    </div>
+                </div>
+            </a>
+            <style>
+                .card-href {
+                    text-decoration: none;
+                    color: #000;
+                }
+
+                .card-container, .card-container-final {
+                    display: flex;
+                    align-items: center;
+                    border-top: 2px solid rgba(0,0,0,0.15);
+                    margin: 0px 0;
+                    padding: 15px 15px;
+                    cursor: pointer;
+                    transition-duration: .15s;
+                }
+
+                .card-container:hover, .card-container-final:hover {                
+                    background: rgb(240, 240, 240);
+                    transition-duration: .15s;
+                }
+
+                .card-container-final {
+                    border-bottom: 2px solid rgba(0,0,0,0.15);
+                }
+
+                .card-thumbnail {
+                    width: 110px;
+                    height: 120px;
+                    object-fit: cover;
+                    margin-right: 20px;
+                    border-radius: 5px;
+                }
+
+                .card-title {
+                    font-weight: bold;
+                    -webkit-margin-before: .4rem;
+                    -webkit-margin-after: .5rem;
+                }
+
+                .card-desc {
+                    color: #757575;
+                    font-size: .85em;
+                }
+
+                .card-sold {
+                    font-size: .85em;
+                    margin-block-start: 1em;
+                    margin-block-end: .4rem;
+                }
+            </style>
+        `;
+    }
+}
+
+customElements.define("list-card", ListCard);
---
+++ pages/list.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/list.css">
+    <script src="../components/navbar.js" type="text/javascript" defer></script>
+    <script src="../components/list-card.js" type="text/javascript" defer></script>
+    <title>Doroyaki</title>
+</head>
+
+<body>
+    <?php
+        echo "<navbar-component></navbar-component>";
+    ?>
+    <div class="container">
+        <h2 class="list-title">Hasil Pencarian</h2>
+        <?php
+            for ($i=0;$i<3;$i++) {
+                $isFinalIndex = $i == 2;
+                echo "<list-card final=$isFinalIndex></list-card>";
+            }
+        ?>
+    </div>
+</body>
+
+</html>
