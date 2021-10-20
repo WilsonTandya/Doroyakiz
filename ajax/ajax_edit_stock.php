ae2435cf0f5ee3a84af647b27a4c5071cc9ede00
WilsonTandya <wilson.tandya@gmail.com>
2021-10-21 00:22:24 +0700

    implement ajax in edit stock function

---
+++ ajax/ajax_edit_stock.php
@@ -0,0 +1,12 @@
+<?php
+    require_once "../app/dorayaki.php";
+    require_once "../pages/util.php";
+
+    $dorayaki = new Dorayaki();
+
+    if (isset($_POST['id']) && isset($_POST['userid']) && isset($_POST['qty'])) {
+        $dorayaki->change_stock($_POST['id'],$_POST['userid'],$_POST['qty']);
+        $res = $dorayaki->detail($_POST['id']);
+        echo $res->STOCK . " buah";
+    }
+?>
--- pages/edit_stock.php
+++ pages/edit_stock.php
@@ -6,25 +6,17 @@
 ?>
 
 <?php
-require_once "../app/dorayaki.php";
-require_once "util.php";
+    require_once "../app/dorayaki.php";
+    require_once "util.php";
 
-$dorayaki = new Dorayaki();
+    $dorayaki = new Dorayaki();
 
-if (isset($_GET['id'])){
-    $id = $_GET['id'];
-    $res = $dorayaki->detail($id);
-}
-
-// REMINDER- static
-$userid = 5;
-
-if (isset($_POST['quantity'])){
-    $res2 = $dorayaki->change_stock($id,$userid,$_POST['quantity']);
-    if($res2) {
-        header("Location: " . "index.php");
+    if (isset($_GET['id'])){
+        $id = $_GET['id'];
+        $res = $dorayaki->detail($id);
     }
-}
+
+    $userid = $_SESSION["user"]["id"];
 
 ?>
 
@@ -40,6 +32,7 @@ if (isset($_POST['quantity'])){
     <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/global.css">
     <link rel="stylesheet" href="../styles/purchase.css">
+    <link rel="stylesheet" href="../styles/not-available.css">
     <script src="../components/navbar.js" type="text/javascript" defer></script>
     <title>Doroyaki</title>
 </head>
@@ -56,7 +49,7 @@ if (isset($_POST['quantity'])){
             <h2 class="page-header">Ubah Stok</h2>
         </div>
         <div class="row">
-            <img class="purchase-image" src="../assets/dorayaki.jpeg" alt="Dorayaki" />
+            <img class="purchase-image" src=<?php echo "../assets/dorayaki/" . $res->IMG_FILE ?> alt="Dorayaki" />
             <div class="purchase-info">
                 <h4 class="purchase-name"><?php echo $res->NAME ?></h4>
                 <p class="purchase-stock">Stok: <span class="subtitle" name="purchase-stock"><?php echo $res->STOCK ?>
@@ -65,16 +58,48 @@ if (isset($_POST['quantity'])){
                     <p class="purchase-price" style="margin-right: auto; margin-top: 25px;">Jumlah Stok Baru</p>
                     <form class="purchase-form" id="purchase-form" action=<?php echo "edit_stock.php?id=" . $id ?>
                         method="post">
-                        <input type="number" name="quantity" min="0" step="1" value="1" />
+                        <input type="number" name="quantity" min="1" step="1" value="1" />
                     </form>
                 </div>
                 <hr class="solid">
-                <button name="submit-button" type="submit" form="purchase-form">
+                <button name="submit-button" onclick="ubah_stok(event)">
                     Ubah Stok
                 </button>
+                <p class="purchase-success"></p>
             </div>
         </div>
     </div>
     </div>
 </body>
+
+<script>
+    function ubah_stok(event) {
+    let xhttp = new XMLHttpRequest();
+    let qtyVal = document.getElementsByName("quantity")[0].value;
+    let qty = qtyVal ? qtyVal : 0;
+    let id = <?php echo $id; ?>;
+    let userid = <?php echo $userid; ?>;
+    let param = `id=${id}&userid=${userid}&qty=${qty}`;
+    xhttp.onreadystatechange = function() {
+        let btnSubmit = document.getElementsByName("submit-button")[0]
+        let textPurchaseSuccess = document.getElementsByClassName("purchase-success")[0]
+        btnSubmit.innerHTML = "Sedang mengubah..."
+        btnSubmit.disabled = true
+        btnSubmit.style.cursor = "not-allowed"
+        btnSubmit.style.background = "rgba(0,0,0,0.3)"
+        textPurchaseSuccess.innerHTML = "Memproses"
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementsByName("purchase-stock")[0].innerHTML = this.responseText
+            textPurchaseSuccess.innerHTML = `*Stok dorayaki berhasil diubah!`
+            btnSubmit.innerHTML = "Ubah Stok"
+            btnSubmit.disabled = false
+            btnSubmit.style.cursor = "pointer"
+            btnSubmit.style.background = "#45b54a"
+        }
+    };
+    xhttp.open("POST", "../ajax/ajax_edit_stock.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(param);
+}
+</script>
 </html>
