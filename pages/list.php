5693bc11037db41bc1a799a8d16123090953d562
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-10-21 11:29:39 +0800

    fix(List): fix 1 page pagination next buttonn bug

--- pages/list.php
+++ pages/list.php
@@ -104,7 +104,7 @@ $res = $dorayaki->search(strtolower($query),$offset,$n_records_per_page);
                     echo "<button class='btn-pagination next-inactive' onclick='updateDorayakiList(" . '"next"' . ")''>&raquo;</button>";
                 } else if ($total_pages == 1) {
                     echo "<button class='btn-pagination selected' onclick='updateDorayakiList(1)'>1</button>";
-                    echo "<button class='btn-pagination next-inactive' onclick='updateDorayakiList(" . '"next"' . ")''>&raquo;</button>";
+                    echo "<button disabled class='btn-pagination next-inactive' onclick='updateDorayakiList(" . '"next"' . ")''>&raquo;</button>";
                 } else {
                     echo "<button class='btn-pagination' onclick='updateDorayakiList(" . '"last"' . ")'>...</button>";
                     echo "<button class='btn-pagination next' onclick='updateDorayakiList(" . '"next"' . ")''>&raquo;</button>";
