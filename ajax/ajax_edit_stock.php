9a9bf6d6dd5c23671dd491ae4a7b22902564901f
WilsonTandya <wilson.tandya@gmail.com>
2021-10-21 20:18:23 +0700

    fix negative edit stock value

--- ajax/ajax_edit_stock.php
+++ ajax/ajax_edit_stock.php
@@ -4,7 +4,10 @@
 
     $dorayaki = new Dorayaki();
 
-    if (isset($_POST['id']) && isset($_POST['userid']) && isset($_POST['qty'])) {
+    if (isset($_POST['id']) && isset($_POST['userid']) && isset($_POST['qty']) && ($_POST["qty"] < 0)) {
+        echo "quantity-invalid";
+    }
+    else if (isset($_POST['id']) && isset($_POST['userid']) && isset($_POST['qty'])) {
         $dorayaki->change_stock($_POST['id'],$_POST['userid'],$_POST['qty']);
         $res = $dorayaki->detail($_POST['id']);
         echo $res->STOCK . " buah";
